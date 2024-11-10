7935dbec2a24e4f44cdf611ec51483e7c56dcaac
🐈‍⬛ㅤ <157797508+Agent-cat@users.noreply.github.com>
2024-11-10 14:57:18 +0530

    Update auth.controllers.js

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../Models/user.model.js");
-const generateToken = require("../config/generateToken.js");
+
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
